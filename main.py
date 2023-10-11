4f43edffc8f30857c999c5edfa4dd8a328c6a369
generalkatony <generalkatony@gmail.com>
2023-10-11 09:54:33 +0100

    removed print message

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import pandas as pd
+import requests
+import json
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+url = "https://app.molecule.io/api/v2/inventory/tickets"
+
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "x-email": "ccheong@qsol.co.uk",
+    "x-token": "cYmDx-EcNdgwqepjUM5j"
+}
+
+df = pd.read_csv(r"C:\Users\cheng\PycharmProjects\MoleculeTicketInjection\Test Data\InvenTest.csv", header=0)
+print(df)
+df = df.drop('id', axis=1)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Convert TRADE_DATE column to desired format
 
+df['as_of'] = pd.to_datetime(df['as_of']).dt.strftime('%d/%m/%y')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(df)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+payload = {
+    "fulfillment_date": "2023-10-09 11:48:43.930787",
+    "commodity": "NG",
+    "volume": 100,
+    "price": 80
+}
+
+response = requests.post(url, json=payload, headers=headers)
+print(response.text)
